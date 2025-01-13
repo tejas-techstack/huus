cd5af263d52017491fb4830a056ab0efe8b4ed88
Tejas R <tejas.techstack@gmail.com>
2025-01-13 15:52:01 +0530

    updated kv

--- kv/storage.go
+++ kv/storage.go
@@ -1,3 +1,18 @@
 // storage.go
 // Used to store data page wise
 package kv
+
+type storage struct {
+  file *file
+
+  pageSize uint16
+
+  // maintain array of freepages.
+  freePages []uint32
+
+  lastPageId uint32
+}
+
+func newStorage (path string, pageSize uint16) (*storage, error){}
+
+func loadMetadata() (*treeMetadata, error) {}
